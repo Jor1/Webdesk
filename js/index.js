8e32e022e686fc424f0448da48e9ac28c968ef8c
赵瑞 <zhaorui@testin.cn>
2016-12-21 17:41:37 +0800

    js优化

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ function checkTime(i)
     {i="0" + i}
     return i
 }
+
 $(function(){
     $(document).ready(function(){
         $("#loading").hover(function(){
@@ -30,72 +31,41 @@ $(function(){
     $("*").mousedown(function(e){
 //                alert(e.which) // 1 = 鼠标左键 left; 2 = 鼠标中键; 3 = 鼠标右键
         return false;//阻止链接跳转
-
     });
     $("#desk ul,.bottom").mousedown(function(e){
         if(1 == e.which){
             $("#bottom-set,#bg-set").hide()
-
-
         }
     });
     $(".bottom").mousedown(function(e){
         if(3 == e.which){
-//                    alert("这 是右键单击事件");
-//                    $(this).css({background:" none repeat scroll 0 0 rgba(124,201,252, 0.9)"})
             var div = $("#bottom-set");
             if(div.is(":hidden")){
                 div.show();
-
-//       div.style.left=document.body.scrollLeft+event.clientX+10;  
-//       div.style.top=document.body.scrollLeft+event.clientY+10;  
                 div.css("left",document.body.scrollLeft+event.clientX+1);
                 div.css("top",document.body.scrollLeft+event.clientY-200);
                 div.css("background-color","#EFF7FE");
             }else{
                 div.hide();
             }}
-
-//                }else if(1 == e.which){
-////                    alert("这 是左键单击事件");
-//
-//
-//                }
     });
     $("#desk ul").mousedown(function(e){
         if(3 == e.which){
-//                    alert("这 是右键单击事件");
-//                    $(this).css({background:" none repeat scroll 0 0 rgba(124,201,252, 0.9)"})
             var div = $("#bg-set");
             if(div.is(":hidden")){
                 div.show();
-
-//       div.style.left=document.body.scrollLeft+event.clientX+10;  
-//       div.style.top=document.body.scrollLeft+event.clientY+10;  
                 div.css("left",document.body.scrollLeft+event.clientX+1);
                 div.css("top",document.body.scrollLeft+event.clientY+1);
                 div.css("background-color","#EFF7FE");
             }else{
                 div.hide();
             }}
-
-//                }else if(1 == e.which){
-////                    alert("这 是左键单击事件");
-//
-//
-//                }
-    })
+    });
     $("#bottom-set li").click(function(){
         var bbg=$(this).attr("style");
 //                alert(bbg);
         $(".bottom").attr("style",bbg)
     });
-//  $("#bg-set img").hover(function(){
-//      $(this).animate({width:600,height:370,position:"absolute",marginLeft:"-50px",marginTop:"0"},0).siblings("img").hide()
-//  },function(){
-//      $(this).animate({width:162,height:97,position:"relative",marginLeft:"0px",marginTop:"0px"},0).siblings("img").show()
-//  })
-
     $("#bg-set img").click(function(){
         var dbg=$(this).attr("src");
 //                alert(dbg)
@@ -182,10 +152,6 @@ $(function(){
         $("#frame-bird").attr("src","")
     });
 
-
-
-
-
     $(".close-window").click(function(){
         $(".window").hide();
         $(".bottom-ul").hide();
@@ -222,22 +188,13 @@ $(function(){
         $("#window-safe").show()
     });
     $('#window-safe').click(function(){
-
         $(this).hide();
         $(".bottom-ul").hide()
-
     });
     $(".wy-music").click(function(){
         $(".my-music").show()
     });
     $(".close-music").click(function(){
         $(".my-music").hide()
-    })
- 
-    
-
-
-
-
-
+    });
 });
